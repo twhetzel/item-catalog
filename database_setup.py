81b2e1a62120cc4a12cf51bb880495fcede0b01b
Trish Whetzel <plwhetzel@gmail.com>
2015-10-26 11:10:49 -0700

    updates to include event url and image url

--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
+from datetime import datetime, date
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -52,10 +53,12 @@ class Event(Base):
     id = Column(Integer, primary_key = True)
     name =Column(String(80), nullable = False)
     description = Column(String(250))
-    #image_url = Column(String(250))
+    event_date = Column(Date)
+    event_url = Column(String(80))
+    image_url = Column(String(250))
     city_id = Column(Integer,ForeignKey('city.id'))
-    #city = relationship(City)
-    city = relationship("City", backref=backref("city", cascade="all, delete-orphan"))
+    city = relationship(City)
+    #city = relationship("City", backref=backref("city", cascade="all, delete-orphan"))
     user_id = Column(Integer,ForeignKey('user.id'))
     user = relationship(User)
 
@@ -66,11 +69,13 @@ class Event(Base):
            'id': self.id,
            'name': self.name,
            'description': self.description,
+           'event_date': str(self.event_date),
+           'event_url': self.event_url,
            'user_id': self.user_id   
         }
 
 
 engine = create_engine('sqlite:///androidevents.db', echo=True)
- 
 
+# Create tables
 Base.metadata.create_all(engine)
